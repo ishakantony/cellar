622e855608feeb9f5fadc307a04aef0d00a34509
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-17 00:48:39 +0800

    config: update Storybook main config for Next.js Vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,18 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
-  ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
+  framework: {
+    name: '@storybook/nextjs-vite',
+    options: {},
+  },
 };
+
 export default config;
