9ba37436d4e5fc594a3ccd472e0ecb3d7f964102
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-17 00:52:48 +0800

    fix: correct Storybook addons to match spec

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
     '@storybook/addon-onboarding',
   ],
   framework: {
