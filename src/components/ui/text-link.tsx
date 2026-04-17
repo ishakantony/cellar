35ce1f5497044bf115863f14639df3911ec3ccf4
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-17 11:54:26 +0800

    feat: add TextLink component with Storybook stories

---
+++ src/components/ui/text-link.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+export interface TextLinkProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function TextLink({ href, children, className }: TextLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "text-primary hover:text-primary-dim transition-colors",
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
