75e7ab6c0fd61b7e96755d63f600b225ebb8b69a
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-17 15:34:07 +0800

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
+import prettier from 'eslint-config-prettier';
 
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -11,14 +12,16 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
     // Coverage reports
-    "coverage/**",
+    'coverage/**',
   ]),
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs['flat/recommended'],
+  // Prettier config must be last to override conflicting ESLint rules
+  prettier,
 ]);
 
 export default eslintConfig;
