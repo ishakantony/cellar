40b4551e847f37eb40e911842b3457103536c709
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-17 11:57:23 +0800

    feat: add LogoIcon auth atom with Storybook stories

---
+++ src/components/auth/logo-icon.tsx
@@ -0,0 +1,19 @@
+import { Package } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export interface LogoIconProps {
+  className?: string;
+}
+
+export function LogoIcon({ className }: LogoIconProps) {
+  return (
+    <div
+      className={cn(
+        "flex h-10 w-10 items-center justify-center rounded-lg bg-primary-container text-on-primary-container",
+        className
+      )}
+    >
+      <Package className="h-5 w-5" />
+    </div>
+  );
+}
